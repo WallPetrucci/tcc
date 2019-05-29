9103a75ff443b6fb27edb81f81ef3078d6ca5934
Higor Oliveira <higoroliveiiira@gmail.com>
2019-05-29 09:34:52 -0300

    Create card component structure

--- frontend/src/components/ContentComponent.vue
+++ frontend/src/components/ContentComponent.vue
@@ -1,72 +1,78 @@
 <template>
   <v-content>
-      <v-container fluid fill-height>
-        <v-layout justify-center align-center>
-          <v-flex text-xs-center>
-          </v-flex>
-        </v-layout>
-      </v-container>
-    </v-content>
+    <v-container fluid fill-height>
+      <v-layout justify-center align-center>
+        <v-flex text-xs-center>
+          <CardComponent></CardComponent>
+          <CardComponent></CardComponent>
+          <CardComponent></CardComponent>
+        </v-flex>
+      </v-layout>
+    </v-container>
+  </v-content>
 </template>
 
 <script type="text/javascript">
-  
-  export default {
-    name: 'ContentComponent',
-    data(){
-      return{
-        ecosystem: [
+import CardComponent from "./CardComponent";
+export default {
+  name: "ContentComponent",
+  components: {
+    CardComponent
+  },
+  data() {
+    return {
+      ecosystem: [
         {
-          text: 'vuetify-loader',
-          href: 'https://github.com/vuetifyjs/vuetify-loader'
+          text: "vuetify-loader",
+          href: "https://github.com/vuetifyjs/vuetify-loader"
         },
         {
-          text: 'github',
-          href: 'https://github.com/vuetifyjs/vuetify'
+          text: "github",
+          href: "https://github.com/vuetifyjs/vuetify"
         },
         {
-          text: 'awesome-vuetify',
-          href: 'https://github.com/vuetifyjs/awesome-vuetify'
+          text: "awesome-vuetify",
+          href: "https://github.com/vuetifyjs/awesome-vuetify"
         }
-        ],
-        importantLinks: [
+      ],
+      importantLinks: [
         {
-          text: 'Documentation',
-          href: 'https://vuetifyjs.com'
+          text: "Documentation",
+          href: "https://vuetifyjs.com"
         },
         {
-          text: 'Chat',
-          href: 'https://community.vuetifyjs.com'
+          text: "Chat",
+          href: "https://community.vuetifyjs.com"
         },
         {
-          text: 'Made with Vuetify',
-          href: 'https://madewithvuetifyjs.com'
+          text: "Made with Vuetify",
+          href: "https://madewithvuetifyjs.com"
         },
         {
-          text: 'Twitter',
-          href: 'https://twitter.com/vuetifyjs'
+          text: "Twitter",
+          href: "https://twitter.com/vuetifyjs"
         },
         {
-          text: 'Articles',
-          href: 'https://medium.com/vuetify'
+          text: "Articles",
+          href: "https://medium.com/vuetify"
         }
-        ],
-        whatsNext: [
+      ],
+      whatsNext: [
         {
-          text: 'Explore components',
-          href: 'https://vuetifyjs.com/components/api-explorer'
+          text: "Explore components",
+          href: "https://vuetifyjs.com/components/api-explorer"
         },
         {
-          text: 'Select a layout',
-          href: 'https://vuetifyjs.com/layout/pre-defined'
+          text: "Select a layout",
+          href: "https://vuetifyjs.com/layout/pre-defined"
         },
         {
-          text: 'Frequently Asked Questions',
-          href: 'https://vuetifyjs.com/getting-started/frequently-asked-questions'
+          text: "Frequently Asked Questions",
+          href:
+            "https://vuetifyjs.com/getting-started/frequently-asked-questions"
         }
-
-        ]
-      }
-    }
+      ]
+    };
   }
+};
 </script>
