71bd60639c84a8457a3f23d86f2e0547f598be36
wallace-notebook <wallacepetrucci@gmail.com>
2019-06-07 20:46:18 -0300

    Change code rasp

--- whm/client.py
+++ whm/client.py
@@ -2,78 +2,69 @@ from socketio.exceptions import ConnectionError as WHMConnectionError
 # from temp import MLX90614
 from socketio import Client
 from time import sleep
-from json import dump, load
 from datetime import datetime
+from uuid import getnode as get_mac
+
 
 import constants as const
 import random
 
-
-conn_status = False
-status_message = False
-
 sio = Client()
-data_client = list()
+data_client = []
 # sensor_temperatura = MLX90614()
-
-
-# Methods for Controller sensor when connect off #
-def escrever_json(res):
-    with open('sensor.json', 'a') as f:
-        dump(res, f, separators=(', ', ': '))
-
-
-def ler_json():
-    with open('sensor.json') as f:
-        data = load(f)
-    return data
+status_message = False
+mac = get_mac()
 
 
 # Callbacks SocketIO
 @sio.on('connect', namespace="/whm")
 def on_connect():
+    global status_message
     print('CONNECT ON')
-    global conn_status, status_message
-    conn_status = True
     status_message = True
 
 
 @sio.on('disconnect', namespace="/whm")
 def on_disconnect():
+    global status_message
     print("CONNECT OFF")
-    global conn_status, status_message
-    conn_status = False
     status_message = False
 
 
-class SocketWhm():
-    def __init__(self, host, port):
-        sio.connect('http://{}:{}'.format(host, port))
+# Functions call socket.IO
 
-    def send_message(self, sensor_data):
-        try:
-            print('Chega aqui')
-            data_client.append(sensor_data)
-            sio.emit('message', data_client, namespace="/whm")
-            data_client.clear()
-        except WHMConnectionError as e:
-            print("Disconnect Error: ", e)
+def connect_socket(host=const.HOST_LOCAL, port=const.PORT_LOCAL):
+    sio.connect('http://{}:{}'.format(host, port))
 
 
-socketobj = SocketWhm(const.HOST_LOCAL, const.PORT_LOCAL)
+def send_message(sensor_data):
+    try:
+        print('Send Message for Server')
+        data_client.append(sensor_data)
+        sio.emit('message', data_client, namespace="/whm")
+        data_client.clear()
+    except WHMConnectionError as e:
+        print("Disconnect Error: ", e)
 
-try:
-    while True:
-        current_date = datetime.now()
-        if status_message is True:
-            sio.start_background_task(socketobj.send_message({'whm_id': "09123901823902",
-                                                              'fc': random.randrange(60, 120),
-                                                              'ox': random.randrange(96, 100),
-                                                              'temp': random.randrange(35, 40),
-                                                              'date': current_date.strftime('%d/%m/%Y %H:%M')}))
-        else:
-            escrever_json({'whm_id': "09123901823902", 'fc': 80, 'ox': 98, 'temp': 37,
-                           'date': current_date.strftime('%d/%m/%Y %H:%M')})
-        sleep(2)
-except:
-    print("Deu ruim")
+
+if __name__ == '__main__':
+    connect_socket(const.HOST, const.PORT)
+    try:
+        while True:
+            current_date = datetime.now()
+            if status_message is True:
+                sio.start_background_task(send_message({'whm_id': mac,
+                                                        'fc': random.randrange(60, 120),
+                                                        'ox': random.randrange(96, 100),
+                                                        'temp': random.randrange(35, 40),
+                                                        'date': current_date.strftime('%d/%m/%Y %H:%M')}))
+            else:
+                print("Save in Database Local: ", {'whm_id': mac,
+                                                   'fc': random.randrange(60, 120),
+                                                   'ox': random.randrange(96, 100),
+                                                   'temp': random.randrange(35, 40),
+                                                   'date': current_date.strftime('%d/%m/%Y %H:%M')})
+
+            sleep(2)
+    except:
+        print("Module not function")
