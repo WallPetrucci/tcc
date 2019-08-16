8b48441bc683de88c533545317e5bd999510294e
Higor Freitas de Oliveira <higoroliveiiira@gmail.com>
2019-08-15 21:22:30 -0300

    Começando adicionar alertas no component de sidebar

--- frontend/src/components/components-template/SideBarAlertComponent.vue
+++ frontend/src/components/components-template/SideBarAlertComponent.vue
@@ -1,6 +1,37 @@
 <template>
   <v-navigation-drawer fixed right app>
-    <v-list dense></v-list>
+    <template>
+      <v-layout row>
+        <v-flex xs12 s12>
+          <v-card>
+
+            <v-list three-line>
+              <v-list-tile>
+                <v-list-tile-action>
+                  <v-icon color="indigo">fas fa-exclamation-circle</v-icon>
+                </v-list-tile-action>
+
+                <v-list-tile-content>
+                 <v-toolbar-title>Alertas</v-toolbar-title>
+                </v-list-tile-content>
+              </v-list-tile>
+              <v-divider ></v-divider>
+
+              <v-list-tile>
+                <v-list-tile-action>
+
+                  <v-list-tile-title>(323) 555-6789</v-list-tile-title>
+                  <v-list-tile-content>Work</v-list-tile-content>
+
+                </v-list-tile-action>
+              </v-list-tile>
+
+              <v-divider inset></v-divider>
+            </v-list>
+          </v-card>
+        </v-flex>
+      </v-layout>
+    </template>
   </v-navigation-drawer>
 </template>
 
