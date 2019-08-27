4dc190c0e05afeea336f17f3bce3601604053092
Wallace - Notebook <wallacepetrucci@gmail.com>
2019-08-26 23:33:12 -0300

    conf

--- backend/config.py
+++ backend/config.py
@@ -1,7 +1,5 @@
-# import os
+import os
 
-# CONNECT_DB = 'mysql+pymysql://{user}:{pw}:@{host}/mydb'.format(user=os.environ['WHM_DB_USER'],
-#                                                                pw=os.environ['WHM_DB_PW'],
-#                                                                host=os.environ['WHM_DB_HOST'])
-
-CONNECT_DB = 'mysql+pymysql://adminwhm:=|?^+6lgkUc)W|hq73e-,6Oz,$C2oAT::@ls-d6fbf83a39a98a7c4563dda058a426b8835ad582.cvbhjerykxan.us-east-1.rds.amazonaws.com/mydb'
+CONNECT_DB = 'mysql+pymysql://{user}:{pw}:@{host}/mydb'.format(user=os.environ['WHM_DB_USER'],
+                                                               pw=os.environ['WHM_DB_PW'],
+                                                               host=os.environ['WHM_DB_HOST'])
