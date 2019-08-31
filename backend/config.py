49e9bd8e58fe213821a1822b5e4b73f5ad6f3e84
Higor <higoroliveiiira@gmail.com>
2019-08-30 21:48:10 -0300

    Alterando gitignore

--- backend/config.py
+++ backend/config.py
@@ -1,3 +1,5 @@
 import os
 
-CONNECT_DB = 'mysql+pymysql://higor:@localhost/mydb'
+CONNECT_DB = 'mysql+pymysql://{user}:{pw}@{host}/mydb'.format(user=os.environ['WHM_DB_USER'],
+                                                               pw=os.environ['WHM_DB_PW'],
+                                                               host=os.environ['WHM_DB_HOST'])
