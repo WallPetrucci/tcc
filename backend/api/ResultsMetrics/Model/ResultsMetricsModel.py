ce426785e38c5898f8b23f3d595627b7abd6363b
Higor <higoroliveiiira@gmail.com>
2019-09-14 19:44:30 -0300

    Rota get para buscar dados dos result metrics dos ultimos 30 dias

--- backend/api/ResultsMetrics/Model/ResultsMetricsModel.py
+++ backend/api/ResultsMetrics/Model/ResultsMetricsModel.py
@@ -1,5 +1,7 @@
 from backend.api.model_base import ResultsMetricsBase
 from backend import db
+from datetime import datetime, timedelta
+from backend.api.ResultsMetrics import constants as const
 
 
 class ResultsMetricsModel(ResultsMetricsBase):
@@ -11,3 +13,25 @@ class ResultsMetricsModel(ResultsMetricsBase):
         # db.session.
         # db.session.commit()
         db.engine.execute(self.__table__.insert(), list_objects)
+
+    @staticmethod
+    def get_result_metrics(id_user):
+        today_date = datetime.now()
+
+        database_result_metrics = db.session.query(
+            ResultsMetricsModel.date_results,
+            db.func.avg(ResultsMetricsModel.oximetry),
+            db.func.avg(ResultsMetricsModel.heart),
+            db.func.avg(ResultsMetricsModel.temperature)
+        ).filter(
+            ResultsMetricsModel.date_results.between(today_date - timedelta(days=const.RESULT_METRICS_DAY), today_date)
+        ).filter_by(User_idUser=id_user).group_by(db.func.cast(ResultsMetricsModel.date_results, db.Date)).all()
+
+        result_metrics = {'oximetry': [], 'heart': [], 'temperature': []}
+
+        for result in database_result_metrics:
+            result_metrics.get('oximetry').append({'data': result[0].strftime("%d-%m-%Y"), 'result': result[1]})
+            result_metrics.get('heart').append({'data': result[0].strftime("%d-%m-%Y"), 'result': result[2]})
+            result_metrics.get('temperature').append({'data': result[0].strftime("%d-%m-%Y"), 'result': result[3]})
+
+        return result_metrics
--- backend/api/ResultsMetrics/constants.py
+++ backend/api/ResultsMetrics/constants.py
@@ -1,1 +1,3 @@
 LIST_KEYS_VALIDATIONS_RESULTS = ['ox', 'temp', 'fc', 'whm_id', 'date']
+
+RESULT_METRICS_DAY = 30
